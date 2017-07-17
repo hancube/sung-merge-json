24774af9bd7c7d10d102453dd29e5b6a8a2f1793
sulim21 <sung.lim@teamaol.com>
2017-07-17 14:18:28 -0400

    Not go into Array

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function (obj1, obj2) {
     function m(o1, o2) {
       for (var node in o2) {
-        if (typeof o2[node] === 'object' && typeof o1[node] !== 'undefined') {
+        if (toString.call(o2[node]) === '[object Object]' && typeof o1[node] !== 'undefined') {
           o1[node] = m(o1[node], o2[node])
         } else if (typeof o1[node] === 'undefined' || o1[node] === null){
           o1[node] = o2[node]
