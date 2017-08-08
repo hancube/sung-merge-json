cba0ee91b2ca7917a41ee868ba0ca7113ac36fd6
sulim21 <sung.lim@teamaol.com>
2017-08-08 13:46:43 -0400

    Merge array item

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 module.exports = function (obj1, obj2) {
-    function m(o1, o2) {
+    function m(o1, o2) { // o1:params,o2:default_values
       for (var node in o2) {
         if (toString.call(o2[node]) === '[object Object]' && typeof o1[node] !== 'undefined') {
           o1[node] = m(o1[node], o2[node])
+        }else if (toString.call(o2[node]) === '[object Array]' && typeof o1[node] !== 'undefined') {
+          for (var i in o1[node]) {
+            o1[node][i] = m(o1[node][i], o2[node][0])
+          }
         } else if (typeof o1[node] === 'undefined' || o1[node] === null){
           o1[node] = o2[node]
         }
